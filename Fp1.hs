6202516f5df5cfede0f9fd515e5ff21b8b0e3655
SashaOrlova <sasha.orlova.1998n@mail.ru>
2016-11-22 02:17:00 +0700

    Update Fp1.hs

--- Fp1.hs
+++ Fp1.hs
@@ -5,20 +5,21 @@ tail' (_:xs)                 =  xs
 foldl' _ z []                = z
 foldl' f z (x:xs)            = foldl' f (f z x) xs
 
-concat' a b                  = a ++ b
+concat' [] ys                = ys
+concat' (x:xs) ys            = x : (concat' xs ys)
 
 take' 0 _                    = []
-take' n (x:xs)               = [] ++ x : [] ++ (take' (n - 1) xs ) 
+take' n (x:xs)               = concat' [x] (take' (n - 1) xs ) 
 
-drop' 1 (x:xs)               = xs
+drop' 0 xs                   = xs
 drop' n (x:xs)               = drop' (n - 1) xs
 
-filter' p []                 = []
+filter' _ []                 = []
 filter' p (x:xs) | p x       = x : filter' p xs
                  | otherwise = filter' p xs
 
-quickSort' []                = []
-quickSort' (x:xs)            = quickSort' small ++ (x : quickSort' large)
+quickSort' []                 = []
+quickSort' (x:xs)             = concat' (quickSort' small) (x : quickSort' large)
      where
           small = [y | y <- xs, y <= x]
           large = [y | y <- xs, y > x]
