6e93b123426aade7f5ee15db6a3c20bdde044c91
SashaOrlova <sasha.orlova.1998n@mail.ru>
2016-12-07 14:23:21 +0700

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,4 +1,4 @@
-select Name from Country 
+select Name, max(LiteracyRate.Rate) from Country, LiteracyRate
 where Code in (select CountryCode from LiteracyRate
 	where Rate in (select max(Rate) from (
 		select max(Year), Rate from LiteracyRate
