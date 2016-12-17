d2204b3418ffaff4c2f43ae4a80bb3c4bf99f1b0
SashaOrlova <sasha.orlova.1998n@mail.ru>
2016-12-18 04:51:40 +0700

    Update static_analyzer.py

--- static_analyzer.py
+++ static_analyzer.py
@@ -19,38 +19,33 @@ class PureCheckVisitor:
         return True
 
     def visit_binary_operation(self, a):
-        return PureCheckVisitor().visit(
-            a.left_part) and PureCheckVisitor().visit(
+        return self.visit(
+            a.left_part) and self.visit(
             a.right_part)
 
     def visit_unary_operation(self, a):
-        return PureCheckVisitor().visit(a.expr)
+        return self.visit(a.expr)
 
     def visit_conditional(self, a):
         tmp = True
-        tmp = tmp and all(
-            list(map(lambda x: PureCheckVisitor().visit(x), a.if_true)))
-        tmp = tmp and all(
-            list(map(lambda x: PureCheckVisitor().visit(x), a.if_false)))
-        tmp = tmp and PureCheckVisitor().visit(a.condtion)
+        tmp = tmp and all([self.visit(x) for x in a.if_true])
+        tmp = tmp and all([self.visit(x) for x in a.if_false])
+        tmp = tmp and self.visit(a.condtion)
         return tmp
 
     def visit_function(self, a):
         tmp = True
-        tmp = tmp and all(
-            list(map(lambda x: PureCheckVisitor().visit(x), a.body)))
-        tmp = tmp and all(
-            list(map(lambda x: PureCheckVisitor().visit(x), a.args)))
+        tmp = tmp and all([self.visit(x) for x in a.body])
+        tmp = tmp and all([self.visit(x) for x in a.args])
         return tmp
 
     def visit_function_definition(self, a):
-        return PureCheckVisitor().visit(a.function)
+        return self.visit(a.function)
 
     def visit_function_call(self, a):
         tmp = True
-        tmp = tmp and all(
-            list(map(lambda x: PureCheckVisitor().visit(x), a.args)))
-        return tmp and PureCheckVisitor().visit(a.expr)
+        tmp = tmp and all([self.visit(x), a.args])
+        return tmp and self.visit(a.expr)
 
 
 class NoReturnValueCheckVisitor:
@@ -59,42 +54,32 @@ class NoReturnValueCheckVisitor:
         return a.accept(self)
 
     def visit_binary_operation(self, a):
-        NoReturnValueCheckVisitor().visit(a.left_part)
-        NoReturnValueCheckVisitor().visit(a.right_part)
+        return self.visit(a.left_part) and self.visit(a.right_part)
 
     def visit_conditional(self, a):
         if (not a.if_true):
-            all(list(map(lambda x: NoReturnValueCheckVisitor().visit(x),
-                         a.if_false)))
+            all([self.visit(x) for x in a.if_false])
             return False
         if (not a.if_false):
-            all(list(map(lambda x: NoReturnValueCheckVisitor().visit(x),
-                         a.if_true)))
+            all([self.visit(x) for x in a.if_true])
             return False
         else:
-            return (all(
-                list(map(
-                    lambda x: NoReturnValueCheckVisitor().visit(x),
-                    a.if_true)))
-                    and all(list(map(
-                        lambda x: NoReturnValueCheckVisitor().visit(x),
-                        a.if_false))))
+            return all([self.visit(x) for x in a.if_true]) and all(
+                [self.visit(x) for x in a.if_false])
 
     def visit_function(self, a):
         if (not a.body):
             return False
         else:
-            return list(
-                map(lambda x: NoReturnValueCheckVisitor().visit(x),
-                    a.body))[-1]
+            return [self.visit(x) for x in a.body][-1]
 
     def visit_function_definition(self, a):
-        if (not NoReturnValueCheckVisitor().visit(a.function)):
+        if (not self.visit(a.function)):
             print(a.name)
         return True
 
     def visit_function_call(self, a):
-        return NoReturnValueCheckVisitor().visit(a.expr)
+        return self.visit(a.expr) and all([self.visit(x) for x in a.args])
 
     def visit_number(self, a):
         return True
@@ -103,10 +88,10 @@ class NoReturnValueCheckVisitor:
         return True
 
     def visit_print(self, a):
-        return True
+        return self.visit(a.expr)
 
     def visit_reference(self, a):
         return True
 
     def visit_unary_operation(self, a):
-        return NoReturnValueCheckVisitor().visit(a.expr)
+        return self.visit(a.expr)
