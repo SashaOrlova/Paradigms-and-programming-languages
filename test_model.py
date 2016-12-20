99601a19f0b99046c74e162a87ecfc599bab3e5b
SashaOrlova <sasha.orlova.1998n@mail.ru>
2016-12-21 04:18:08 +0700

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,3 +1,4 @@
+import pytest
 import os
 import sys
 import io
@@ -24,15 +25,14 @@ class TestConditional:
                         Number(0)])) == 1
 
     def test_simple_empty_conditional(self):
-        assert get_v(Conditional(Number(1), [Number(1)])) == 1
+        get_v(Conditional(Number(1), [Number(1)]))
 
     def test_empty_conditional(self):
-        scope = {}
-        assert get_v(Conditional(Number(1), [], [])) == 0
-        assert get_v(Conditional(Number(0), None)) == 0
-        assert get_v(Conditional(Number(0), [], [Number(1)])) == 1
-        assert get_v(Conditional(Number(1), None, [Number(1)])) == 0
-        assert get_v(Conditional(Number(0), [Number(1)], None)) == 0
+        get_v(Conditional(Number(1), [], []))
+        get_v(Conditional(Number(0), None))
+        get_v(Conditional(Number(0), [], [Number(1)])) 
+        get_v(Conditional(Number(1), None, [Number(1)]))
+        get_v(Conditional(Number(0), [Number(1)], None))
 
 
 class TestFunction:
@@ -41,7 +41,7 @@ class TestFunction:
         assert get_v(Function((), [Number(1)])) == 1
 
     def test_empty_function(self):
-        assert get_v(Function((), [])) == 0
+        get_v(Function((), []))
 
 
 class TestFunctionDefinition:
@@ -55,14 +55,29 @@ class TestFunctionDefinition:
 
 class TestFunctionCall:
 
-    def test_function_call(self):
+    def test_function_call_simple(self):
         scope = {}
         scope["nothing"] = Function((), [Number(1)])
         assert get_v(FunctionCall(
             FunctionDefinition(
                 'nothing', scope['nothing']), [])) == 1
 
-
+    def test_function_call(self):
+        scope = {}
+        scope["multiply_two_numbers"] = Function(
+        ('first_number', 'second_number'), [
+             BinaryOperation(
+                Reference('first_number'), '*',
+                Reference('second_number'))])
+        scope['first'] = Number(3)
+        scope['second'] = Number(4)
+        scope 
+        t = FunctionCall(
+        FunctionDefinition(
+            'multiply_two_numbers', scope['multiply_two_numbers']), [
+            Reference("first"), Reference("second")]).evaluate(scope)
+        assert get_v(t) == 12
+        
 class TestBinaryOperation:
 
     def test_simple_binary_operation(self):
@@ -70,15 +85,38 @@ class TestBinaryOperation:
         assert get_v(BinaryOperation(Number(0), '||', Number(1))) == 1
         assert get_v(BinaryOperation(Number(4), '*', Number(5))) == 20
         assert get_v(BinaryOperation(Number(4), '>', Number(5))) == 0
+        assert get_v(BinaryOperation(Number(6), '-', Number(5))) == 1
+        assert get_v(BinaryOperation(Number(0), '&&', Number(1))) == 0
+        assert get_v(BinaryOperation(Number(4), '<', Number(5))) == 1
+        assert get_v(BinaryOperation(Number(5), '==', Number(5))) == 1
+        assert get_v(BinaryOperation(Number(4), '!=', Number(5))) == 1
+        assert get_v(BinaryOperation(Number(0), '<=', Number(1))) == 1
+        assert get_v(BinaryOperation(Number(4), '>=', Number(5))) == 0
+        assert get_v(BinaryOperation(Number(10), '/', Number(5))) == 2
 
 
 class TestReference:
 
-    def test_reference(self):
+    def test_reference_simple(self):
         scope = {}
         scope['example'] = 123
         assert Reference('example').evaluate(scope) == 123
 
+    def test_reference(self):
+        scope = {}
+        t = Function((), [Number(1)])
+        scope['example'] = t
+        assert Reference('example').evaluate(scope) is t
+
+    def test_reference_and_conditional(self):
+        scope = {}
+        scope["true"] = Number(1)
+        res = Conditional(
+            Reference("true"), [
+                Number(1)], Number(0)).evaluate(scope)
+        assert get_v(res) == 1
+
+        
 
 class TestUnaryOperation:
 
@@ -102,5 +140,39 @@ class TestScope:
         scope = Scope(parent)
         assert scope['foo'] is foo
 
+class TestPrint:
+
+    def test_print_simple(self):
+        scope = {}
+        ex = Number(1);
+        old_stdout = sys.stdout
+        sys.stdout = io.StringIO()
+        Print(ex).evaluate(scope)
+        res = int(sys.stdout.getvalue())
+        sys.stdout = old_stdout
+        assert res == 1
+
+    def test_print_with_evaluate(self):
+        scope = {}
+        ex = BinaryOperation(Number(10), '/', Number(5));
+        old_stdout = sys.stdout
+        sys.stdout = io.StringIO()
+        Print(ex).evaluate(scope)
+        res = int(sys.stdout.getvalue())
+        sys.stdout = old_stdout
+        assert res == 2
+
+
+class TestRead:
+    
+    def test_read_simple(self):
+        scope = {}
+        old_stdin = sys.stdin 
+        sys.stdin = io.StringIO('6')
+        t = Read('a').evaluate(scope)
+        sys.stdin = old_stdin
+        assert get_v(t) == 6 
+
+    
 if __name__ == "__main__":
     pytest.main()
