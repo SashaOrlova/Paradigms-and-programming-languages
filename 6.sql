e6d1648214666ebff668db85f5257716699bf1a0
SashaOrlova <sasha.orlova.1998n@mail.ru>
2016-12-21 21:43:51 +0700

    Update 6.sql

--- 6.sql
+++ 6.sql
@@ -1,4 +1,4 @@
 SELECT City.Name, City.Population, Country.Population FROM City
 INNER JOIN Country ON City.CountryCode = Country.Code
-ORDER BY (100 * City.Population / Country.Population) DESC, City.Name DESC
+ORDER BY (1.0 * City.Population / Country.Population) DESC, City.Name DESC
 LIMIT 20;
